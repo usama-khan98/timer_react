d87d955681f49bcd6f9e95abfb1e9d03a39a95f9
erika-dorset <erika.volodko@faculty.dorset-college.ie>
2020-03-30 14:50:12 +0000

    commit

---
+++ my-react-app/src/index.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Timer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+            countTime: this.props.startCount 
+        };
+    }
+
+    componentDidMount() {
+        // store the timer id to use later to get rid of it
+        this.timerID = setInterval(
+            () => this.setState(prevState => ({
+                countTime: prevState.countTime - 1
+            })
+            ), 1000);
+    }
+
+    componentWillUnmount() {
+        // the time won't be displayed so clear it
+        clearInterval(this.timerID);
+    }
+
+    // return the state value
+    render() {
+        if (this.state.countTime > 0) {
+            return <h2>Time remaining: {this.state.countTime} seconds.</h2>;
+        } else {
+            // the time won't be displayed so clear it
+            clearInterval(this.timerID);
+            return <h2>Time up!!</h2>;
+        }
+    }
+}
+
+ReactDOM.render(
+    <div>
+        <Timer startCount={10} />
+        <Timer startCount={30} />
+        <Timer startCount={60} />
+    </div>,
+    document.getElementById('root')
+);
